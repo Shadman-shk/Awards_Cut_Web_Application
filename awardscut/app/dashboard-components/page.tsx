931b632207d0f73278465a3c358bc4881b615430
Shadman <hossainshadman005@gmail.com>
2026-03-26 02:33:35 +1100

    Setting up the dashboard with parallel routing

---
+++ awardscut/app/dashboard-components/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashComp = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DashComp
---
+++ awardscut/app/dashboard/@recent_events/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Recent = () => {
+  return (
+    <div>
+       <h1>Recent Events</h1>
+    </div>
+  )
+}
+
+export default Recent
